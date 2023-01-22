7938b198d5978b35b95dedb3e6fd71229c8a8282
星芒 <121670656+wlkq1@users.noreply.github.com>
2023-01-22 20:14:51 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+    //加载条
+var pg=document.getElementById('pg'); 
+setInterval(function(e){
+   if(pg.value!=100) pg.value++;      
+   else pg.value=0;
+},100);
     //获得主界面
 var mainDiv=document.getElementById("maindiv");
     //获得开始界面
